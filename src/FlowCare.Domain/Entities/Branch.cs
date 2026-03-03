d93f8eb90f7c077f4a881f532730adc1ce5e08d1
OWAIS <120783442+owais-alhjri@users.noreply.github.com>
2026-03-03 11:57:47 +0400

    I added the branch entity

---
+++ src/FlowCare.Domain/Entities/Branch.cs
@@ -0,0 +1,26 @@
+﻿namespace FlowCare.Domain.Entities;
+
+public class Branch
+{
+    public string Id { get; private set; }
+    public string Name { get; private set; }
+    public string City { get; private set; }
+    public string Address { get; private set; }
+    public string Timezone { get; private set; }
+    public bool IsActive { get; private set; }
+
+    protected Branch()
+    {
+    }
+
+    public Branch(string id, string name, string city, string address, string timezone, bool isActive)
+    {
+        Id = id;
+        Name = name;
+        City = city;
+        Address = address;
+        Timezone = timezone;
+        IsActive = isActive;
+    }
+
+}
