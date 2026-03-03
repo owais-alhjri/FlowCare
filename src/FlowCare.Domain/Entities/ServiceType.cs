59defc86dff2440e3a434d1d3f180396100f9576
OWAIS <120783442+owais-alhjri@users.noreply.github.com>
2026-03-03 12:07:00 +0400

    I added two entities (ServiceType and StaffServiceType)

---
+++ src/FlowCare.Domain/Entities/ServiceType.cs
@@ -0,0 +1,26 @@
+﻿namespace FlowCare.Domain.Entities;
+
+public class ServiceType
+{
+    public string Id { get; private set; }
+    public string BranchId { get; private set; }
+    public string Name{ get; private set; }
+    public string Description{ get; private set; }
+    public int DurationMinutes { get; private set; }
+    public bool IsActive{ get; private set; }
+
+
+    protected ServiceType()
+    {
+    }
+
+    public ServiceType(string id, string branchId, string name, string description, int durationMinutes, bool isActive)
+    {
+        Id = id;
+        BranchId = branchId;
+        Name = name;
+        Description = description;
+        DurationMinutes = durationMinutes;
+        IsActive = isActive;
+    }
+}
---
+++ src/FlowCare.Domain/Entities/StaffServiceType.cs
@@ -0,0 +1,17 @@
+﻿namespace FlowCare.Domain.Entities;
+
+public class StaffServiceType
+{
+    public string StaffId { get; private set; }
+    public string ServiceTypeId { get; private set; }
+
+    protected StaffServiceType()
+    {
+    }
+
+    public StaffServiceType(string staffId, string serviceTypeId)
+    {
+        StaffId = staffId;
+        ServiceTypeId = serviceTypeId;
+    }
+}
